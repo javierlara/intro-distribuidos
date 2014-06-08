70444b91c3e9664055b6941e29d1a12c390ed088
francisco.memoli@gmail.com <francisco.memoli@gmail.com@354cda8d-dc19-acc4-82eb-78b65d4dea2c>
2014-06-08 02:24:29 +0000

    nuevos tuneles para los servers y agregado de la tabla de ruteo

--- trunk/OpenVPN/Clientes/crearInterfazClienteA.sh
+++ trunk/OpenVPN/Clientes/crearInterfazClienteA.sh
@@ -24,4 +24,11 @@ sudo openvpn --rmtun --dev $interfaces
 
 sudo tunctl -t $interfaces
 sudo ifconfig $interfaces promisc
-sudo  openvpn --remote $serverIp --port $miPortA --dev $interfaces --ifconfig $miIPVirtualA $maskA $miIPVirtualDestinoA 
+sudo  openvpn --remote $serverIp --port $miPortA --dev $interfaces --ifconfig $miIPVirtualA $maskA $miIPVirtualDestinoA &
+
+sleep 3
+
+subredes="10.15.65.0/27 10.94.6.128/26 10.94.65.128/25 10.43.9.0/24 205.129.31.128/30 10.15.65.32/27 10.94.5.144/28 205.129.31.132/30 10.15.65.64/27 10.94.5.160/28 10.94.5.176/28 10.15.65.96/27 205.129.31.0/26 10.94.6.192/26 10.94.5.128/28 205.129.31.64/30"
+for subred in $subredes; do
+    sudo route add -net $subred gw 10.94.5.242 $interfaces
+done
