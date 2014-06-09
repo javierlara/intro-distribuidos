6bafc7abe929b67177d1b8e13c7dbcc1be99240f
jlara91@gmail.com <jlara91@gmail.com@354cda8d-dc19-acc4-82eb-78b65d4dea2c>
2014-06-09 23:55:38 +0000

    Subo scripts para iniciar y startear web server

--- trunk/Servers/servers_init.sh
+++ trunk/Servers/servers_init.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 SRV="srv"
 ETC="etc"
+APACHE="apache2"
+VAR="var"
 
 if [ "$(id -u)" != "0" ]; then
    echo "Debe ejecutar como administrador"
@@ -18,3 +20,16 @@ mkdir ~/aux
 	rm -r /$SRV/ftp/*
 
 	cp -r ftp/$SRV/ftp /$SRV/
+	
+	
+	echo "Instalando Apache"	
+	apt-get install apache2
+
+	mkdir /$ETC/$APACHE
+	cp -r /$ETC/$APACHE/ ~/aux/
+	cp -r /$VAR/www/ ~/aux/
+	rm /$VAR/www/*
+	cp -r web/$APACHE/ /$ETC/
+	cp -r web/www/ /$VAR/
+	chown -R $USER:$USER /var/www/
+	chmod -R 755 /var/www/
--- trunk/Servers/servers_start.sh
+++ trunk/Servers/servers_start.sh
@@ -30,4 +30,36 @@ case "$1" in
 		;;
 		esac
 	;;
+	
+	#Apache
+	apache)
+	ps -ef | grep "apache2" | grep -v "grep" > /dev/null
+	if [ "$?" -eq "0" ]
+	then
+		echo "El servidor Apache2 está corriendo."
+		case "$2" in
+		stop)
+			service apache2 stop
+			echo "Stopping web server"
+		;;
+		esac
+		exit
+	fi
+
+	echo "El Servidor Apache2 no esta corriendo."
+	case "$2" in	
+		start)
+			service apache2 start	
+			echo "Iniciando servidor Apache2.."
+		;;
+		esac
+	;;
+
+
+	*)
+	echo "Parametros incorrectos"
+	;;
+	
+	
+	
 esac
