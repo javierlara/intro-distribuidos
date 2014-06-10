e0c6c9874eb2da799d0363e103cd0a523aefe9c0
francisco.memoli@gmail.com <francisco.memoli@gmail.com@354cda8d-dc19-acc4-82eb-78b65d4dea2c>
2014-06-10 19:41:43 +0000

    cambio en openvpn

--- trunk/OpenVPN/Clientes/crearFTP.sh
+++ trunk/OpenVPN/Clientes/crearFTP.sh
@@ -1,4 +1,28 @@
 #!/bin/sh
+if [ "$(id -u)" != "0" ]; then
+	echo "Debe ejecutar como administrador"
+	exit 1
+fi
+
+if ! type "tunctl" > /dev/null; then
+	echo "se tiene que instalar la función tunctl"
+	exit 1
+fi
+if ! type "openvpn" > /dev/null; then
+	echo "se tiene que instalar la función openvpn"
+	exit 1
+fi
+serverConfig="serverIp.conf"
+if [ ! -f "$serverConfig" ]; then
+    echo "El archivo $serverConfig no existe, copiar el $serverConfig.sample a $serverConfig y poner la IP fisica del modelo"
+    exit 1
+fi
+
+# limipo los taps
+echo "Limipiando taps"
+${BASEDIR}/../borrarInterfaces.sh
+
+echo "Iniciando FTP"
 
 interfaces="tap9"
 
@@ -22,8 +46,8 @@ miPortC=34000
 sudo openvpn --rmtun --dev $interfaces
 
 
-sudo tunctl -t $interfaces
 sudo ifconfig $interfaces promisc
+#echo "nameserver 10.134.13.66" > /etc/resolv.conf
 sudo  openvpn --remote $serverIp --port $miPortC --dev $interfaces --ifconfig $miIPVirtualC $maskC $miIPVirtualDestinoC &
 sleep 3
 
