3ac8623ccfe628dec66479916080a275d22e9eac
francisco.memoli@gmail.com <francisco.memoli@gmail.com@354cda8d-dc19-acc4-82eb-78b65d4dea2c>
2014-06-10 22:39:41 +0000

    agrego los archivos de dNS

---
+++ trunk/DNS/crearDNS1.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+#verifico permisos
+if [ "$(id -u)" != "0" ]; then
+	echo "Debe ejecutar como administrador"
+	exit 1
+fi
+
+chequeoDirectorio(){
+	dirName=${1}
+	if [ ! -d "$dirName" ]; then
+	    echo "El directorio $dirName no existe"
+	    exit 1
+	fi
+}
+
+chequeoPrograma(){
+	nombrePrograma=${1}
+	if ! type "$nombrePrograma" > /dev/null; then
+		echo "Se tiene que instalar la función $nombrePrograma"
+		exit 1
+	fi
+}
+function start_bind9 {
+	echo "Starting bind9"
+	/etc/init.d/bind9 restart
+
+	if [ "$?" != "0" ];then 
+		exit_conf "3" "Error iniciando bind9"
+	fi
+	return 0
+}
+#chequeoPrograma "bind"
+
+
+#copiar la configuración a el bind
+
+bindPath="/etc/bind/"
+chequeoDirectorio $bindPath
+	if [ -d "$bindPath*" ]; then
+	    rm -R "$bindPath*"
+	fi
+
+cp bind_dns1/* $bindPath
+
+start_bind9
