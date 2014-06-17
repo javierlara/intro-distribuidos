ff09fae68476174b796e1ba84ae997d4f44f05ef
jlara91@gmail.com <jlara91@gmail.com@354cda8d-dc19-acc4-82eb-78b65d4dea2c>
2014-06-17 18:00:25 +0000

    Fixes en servers

--- Servers/crearTelnet.sh
+++ Servers/crearTelnet.sh
@@ -1,12 +1,23 @@
-ETC="etc"
+#!/bin/sh
+case "$1" in
+	init)
+	ETC="etc"
 	echo "Instalando Telnet"
 	apt-get install telnetd
 	apt-get install xinetd
 	cp -r /$ETC/xinetd.d/ ~/aux/
 	cp Telnet/telnet /$ETC/xinetd.d/
 
-#	service xinetd stop
-
+	;;
+	start)
+	echo "Iniciando Telnet"
 	service xinetd start	
-
+	;;
 	
+	stop)
+	echo "Stopping Telnet"
+	service xinetd stop
+
+	;;
+	
+esac
